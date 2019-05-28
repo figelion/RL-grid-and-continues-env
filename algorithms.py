17678db8b4a9d7e7edb61c310ad31161859b6d03
Adam Bugaj <adam.m.bugaj@gmail.com>
2019-05-28 08:45:41 +0200

    Added algo R

--- algorithms.py
+++ algorithms.py
@@ -44,6 +44,7 @@ class Algorithm:
         """
 
 
+
 class Ahc(Algorithm):
 
     def __init__(self, environment, epsilon, beta, gamma, alpha):
@@ -258,11 +259,68 @@ class QlearningLambda(Algorithm):
 
                     prize = self._env.get_Prize(current_state)
 
+                    for x in range(self._env.size_state):
+                        for a in range(len(self._env.actions)):
+                            if (x, a) == (current_state, action):
+                                self._e[x][a] += 1
+                                continue
+                            self._e[x][a] *= self._gamma * self._lambda
+
                     maxQ_ = max(Q[current_state][:])
                     delta = prize + self._gamma * maxQ_ - self._Q[previous_state][action - 1]
 
-                    self._Q[previous_state][action - 1] = self._Q[previous_state][action-1] \
-                                                            + self._alpha * delta
+                    self._Q[previous_state][action - 1] += self._alpha * delta \
+                                                            * self._e[previous_state][action - 1]
+
+                    if self._env.is_absorbing_state(self._rout, current_state):
+                        self.data[episode][measurement] = len(self._rout)
+                        break
+
+
+class R (Algorithm):
+    def __init__(self, grid_environment,  epsilon, beta, gamma, alpha):
+        super().__init__(grid_environment, epsilon, beta, gamma, alpha)
+        self._Q = []
+        self.ro = 0
+
+    def _make_action(self, state):
+        A = self._env.actions
+        greedy_action = []
+        max_Q = max(self._Q[state][:])
+        for x in range(len(self._actions)):
+            if self._Q[state][x] == max_Q:
+                # +1 because A = [1,2,3,4] and x <0,3>
+                greedy_action.append(x + 1)
+        if random() < self._epsilon:
+            action = A[randint(0, (len(greedy_action) - 1))]
+        else:
+            action = greedy_action[randint(0, (len(greedy_action) - 1))]
+
+        return action, self._env.make_move(action, state)
+
+    def learn(self, size_episodes, size_measurement):
+        for measurement in range(size_measurement):
+            self._Q = np.zeros((self._env.size_state, len(self._actions)))
+
+            for episode in range(size_episodes):
+                current_state = self._env.state
+                self._rout = []
+
+                while True:
+                    previous_state = current_state
+                    action, current_state = self._make_action(current_state)
+                    self._rout.append(action)
+
+                    prize = self._env.get_Prize(current_state)
+                    maxQ_ = max(Q[current_state][:])
+                    maxQ = max(Q[previousPositionX][previousPositionY][:])
+                    value_Q_previous = Q[previousPositionX][previousPositionY][action - 1]
+                    delta = prize - ro + gamma * maxQ_ - value_Q_previous
+
+                    Q[previousPositionX][previousPositionY][action - 1] += alpha*delta
+
+                    if Q[previousPositionX][previousPositionY][action - 1] == maxQ:
+                        ro += beta * (prize - ro + maxQ_ - maxQ)
 
                     if self._env.is_absorbing_state(self._rout, current_state):
                         self.data[episode][measurement] = len(self._rout)
